546b1b2041739ecd173056f08e1e391bda0760c3
Jakob Hilarius <jakob@syscall.dk>
2013-04-21 21:55:56 +0200

    Can actually list lights now

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -3,7 +3,7 @@
 # -
 ####################################################################################################
 
-import ClipAPI
+from clip import *
 
 ART = 'art-default.png'
 ICON = 'icon-default.jpg'
@@ -13,7 +13,6 @@ ICON = 'icon-default.jpg'
 DEVICE_TYPE = 'Plex ControlHue'
 DISCOVERY_BROKER = 'http://www.meethue.com/api/nupnp'
 
-LINK_RETRY_COUNT = 5
 
 def Start():
     ObjectContainer.art = R(ART)
@@ -30,42 +29,51 @@ def MainMenu():
 
     bridges = FindHueBridges()
 
+    Log(bridges)
+
     if len(bridges) < 1:
         return MessageContainer("Error", "Cannot find any Philips Hue bridges on your network")
     else:
         # TODO: len(hues) > 1 -> select controller
         bridge = bridges[0]
 
-        errcnt = 0
-        while (errcnt < LINK_RETRY_COUNT):
-            try:
-                api = ClipAPI(bridge)
-                for light in api.list():
-                    if light.get('active'):
-                        thumb = R("icon-bulb-on.png")
-                    else:
-                        thumb = R("icon-bulb-off.png")
-
-                    oc.add(
-                        DirectoryObject(
-                            key=Callback(ToggleLight, light, api=api),
-                            title=light.get('name'),
-                            thumb=thumb))
-
-                # Break out of our retry loop since we reach here without exceptions
-                break
-            except LinkError as e:
-                errcnt = errcnt + 1
-                return MessageContainer("Error", "Press the link button on your Philips Hue bridge")
+        return ListLights(bridge)
 
     #oc.add(PrefsObject(title="Preferences", summary="Configure how to connect to Trakt.tv", thumb=R("icon-preferences.png")))
+    return oc
+
+
+@route('/applications/controlhue/refresh')
+def Refresh(bridge):
+    return MessageContainer('test', 'test')
+
+
+def ListLights(bridge):
+    api = ClipAPI(bridge)
+
+    oc = ObjectContainer()
+    try:
+        for light in api.list():
+            if light.get('active'):
+                thumb = R("icon-bulb-on.png")
+            else:
+                thumb = R("icon-bulb-off.png")
+
+            oc.add(
+                DirectoryObject(
+                    key=Callback(ToggleLight, light, api=api),
+                    title=light.get('name'),
+                    thumb=thumb))
+    except LinkError:
+        oc.add(DirectoryObject(key=Callback(Refresh, bridge=bridge), title=L("Refresh"), summary=L("Click this after you have pressed the link button on your Hue bridge.")))
+
     return oc
 
 
 def FindHueBridges():
     return JSON.ObjectFromURL(DISCOVERY_BROKER)
 
-
+@route('/applications/controlhue/toggle')
 def ToggleLight(light, api):
     if light.get('active'):
         api.off(light.get('id'))
---
+++ Contents/Code/clip.py
@@ -0,0 +1,91 @@
+class ClipAPI:
+    """ Implementation of the Clip API used by Philips Hue API """
+
+    API_USERNAME = 'testtesttest1'
+
+    # [{'internalipaddress': '10.0.0.35', 'id': '001788fffe096c19', 'macaddress': '00:17:88:09:6c:19'}]
+    def __init__(self, bridge):
+        self.api = 'http://' + bridge['internalipaddress'] + '/api'
+
+    # http://<bridge ip address>/api/newdeveloper/lights
+    # GET
+    def list(self):
+        results = self.request(self.api + '/' + self.API_USERNAME + '/lights')
+
+        lights = []
+
+        #{'1': {'name': 'Miss K'}, '3': {'name': 'Ceiling Lamp'}, '2': {'name': 'AJ Floor'}}
+        for key, value in results.items():
+            lights.append({'id': key, 'name': value['name']})
+
+        # [ { "active": true, "name", "Foo"} ]
+        return lights
+
+    # http://<bridge ip address>/api/newdeveloper/lights/1/state
+    # PUT{"on":true}
+    def on(self, light):
+        self.request(self.api + '/' + self.API_USERNAME + '/lights/' + light + '/state', 'PUT')
+
+    def off(self, light):
+        self.equest(self.api + '/' + self.API_USERNAME + '/lights/' + light + '/state', 'PUT')
+
+    # http://<bridge ip address>/api
+    # POST {"devicetype":"test user","username":"newdeveloper"}
+    def authenticate(self):
+        register = {'devicetype': 'test user', 'username': self.API_USERNAME}
+
+        resp = HTTP.Request(self.api, data=JSON.StringFromObject(register)).content
+
+        Log(resp)
+
+        if self.is_link_error(JSON.ObjectFromString(resp)):
+            raise LinkError('Not linked')
+
+    def request(self, url, method='GET'):
+        results = JSON.ObjectFromURL(url, method=method)
+
+        Log(results)
+
+        if self.is_auth_error(results):
+            self.authenticate()
+            # Retry
+            results = JSON.ObjectFromURL(url, method=method)
+            Log(results)
+
+        if self.is_link_error(results):
+            raise LinkError('Not linked')
+
+        return results
+
+    def is_auth_error(self, data):
+        if len(data):
+            return False
+
+        if 'error' in data[0]:
+            if data[0]['error']['type'] == 1:
+                return True
+        return False
+
+    def is_link_error(self, data):
+        if len(data):
+            return False
+
+        if 'error' in data[0]:
+            if data[0]['error']['type'] == 101:
+                return True
+        return False
+
+    def is_error(self, data):
+        # [ { "error": { "type": 1 } }]
+        if 'error' in data[0]:
+            return True
+
+        return False
+
+
+class LinkError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
